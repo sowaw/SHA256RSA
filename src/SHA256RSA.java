c3ef78d342d8c9d62749007c67defaec6eeedd4c
Wojciech Sowa <wsowa.planer@gmail.com>
2020-05-01 12:44:44 +0200

    Complete class

--- src/SHA256RSA.java
+++ src/SHA256RSA.java
@@ -1,96 +1,77 @@
-import org.json.simple.JSONObject;
-
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
-import java.security.*;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.Signature;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Base64;
 import java.util.Date;
 
+
 public class SHA256RSA {
-/*
-    public static void main(String[] args) throws UnsupportedEncodingException {
-        //Base64 decoder = new Base64();
-        String url = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9";
-        byte[] decoded = Base64.getUrlDecoder().decode(url);
 
-        System.out.println(new String(decoded, "UTF-8") + "\n");
+    private final static String PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEA" +
+            "AoIBAQCpyNSRb8sZTsOW\nqbhKvQ8/v/XSXKSdn3lBXfUgb5P+rX5L2p8MY5ONvOpVBaUpYmAm5f5QLn1fDdOf\nen6ERTdIQMGlRbeCtNH" +
+            "KWRCF1s/loDHapl3nsvu78OHCvoHDsGMsRtTThAYq6jXk\nRLjd6fsUBlMV3IC8XxctNUl8/DrHtSyionLOJfiLp1lD2C7DadeFOwvTOdWo" +
+            "qFE0\nqMg3L3jnd3vl+zo3otnoldYXtUO0AWpjgjqyk/QG/pEVFD9fmLIUpY9X9W4avrPw\nV74dPLI3mE/xzeGR9KzJFOAYQE1Cz1kgSsL" +
+            "fMeOQP8BlWWsO6sBw8/zNWLZV2naQ\nyxk522VrAgMBAAECggEAMwuRuR4SwkuXazsgkLvRk1mLtxCqX0dcZcYxVeyl4opX\nr8eNTOS9Ln5" +
+            "vN1uooc4VWWyJbqLjh4n5J3flBLBoo/zwG1fgCdiCQGNRn+OLAp8M\njg+5qNj7bTBJN8sd6W8lCeFLyEHntsxOoo/0wqc/mPAj161BOSEfr" +
+            "zRXoU+V/odg\n+xb667jWc7mceuxVUtX86etdR/VZH3JSzgyEZ1fZpWLhXutqqelC8NdItMnxYqT1\ntWksPwH7N2wFdXvNyU3kD3EmdCcxE" +
+            "e3H77+zkkdVpgHz3F8p6WW/Qxm3tiWW9JJD\n6gHFwh8I+sR+bFAPnzQZiRAAH6iihkylnMrqrd47ZQKBgQDVxA8cBq2gJ90Vxd7a\noJOI" +
+            "crDJzy56bZ9oZxwtYbUluPLk1GRMkFjncLGeFMhry9Xhnbn4a+PN2zCwj4c/\n61SmBErGhFP6yQ/+qez+kz8PSqA73nk+JM9B6nDix482YK" +
+            "oY5SZymlJ04PnGWbSU\nQi8utmPnsbf8BdkoCDAlWi2GLwKBgQDLVENeiiXpTwHw2iO1FbQ9LsufAZmeMnyC\nBsi1kxNlxw6xGHPSOnG2hO" +
+            "GUp4HXJvcjD8OFYxtB32Q8UilcSPWI1YpzAasJPuxN\nJTZqkUzixf8Tn4BKNjYZEExynUKDsB1VBAMCkW2OqXxIjKQruPfRxGrRNNmCdv" +
+            "EW\nLc5WuxmBhQKBgBENbtchgUBBbdWKz1hJbvt3irrOmDqT/B9r+kd0f+reciHm/4lM\nCouL5d9icQqIXCt/VZKHqMiHL78l8/gZYctMl" +
+            "Fp5u+lJmMkL7SfxvxoNLfMYsDtQ\n3Ge5t/+fQ8FmvKr3vLVvshw4xjQYe9tH3FOxoQ6ekrq8DLKw1IZmRzNpAoGAGhrB\nY3vdDi0klK" +
+            "WM/AzDTS+a2Nk9vb/BmHgCgL1XOjPqQPZguFbkjohU5d7znonUJN47\nPf7RWw2xMsVhpgV/8Idp9QX7zX4UnHrwl5H8CwlcjEpoEB8Rpvo" +
+            "etoBWL84Glgmj\n0UqWhEBiQnY8BZkDyBytkhp06nkWzsAmw5/V0HkCgYAGXKw5B0VhGcJ933aHdvoS\nKZJb4F5EvzjrpFVwDeLIoR64Nt" +
+            "KBEXRtAqrFu3CpiJH3I6snjxTg9sNVcqru4SpL\n/lLfH6/TWVc0VUZsWwJNxLgBScZYliw+DARhNvyf8rgGGgotb8/JcdE5NQyhOlpb\ngF" +
+            "Wx38c5jfNe7ZlpB8eNdw==\n-----END PRIVATE KEY-----\n";
+
+    private final static String RSA_SHA256 = "SHA256withRSA";
+
+    private final static String KEY_TAG_PATTERN = "-----[A-Z ]+-----";
+
+    private final static String RSA_ALGORITH = "RSA";
+
+    private static String getBase64URLString(String str) {
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(str.getBytes(StandardCharsets.UTF_8));
     }
-*/
 
-    public static void main(String[] args) throws Exception {
+    private static byte[] keyStringToBytes(String keyString) {
+        keyString = keyString
+                .replaceAll(KEY_TAG_PATTERN, "")
+                .replaceAll("\\s", "");
 
+        return Base64.getDecoder().decode(keyString);
+    }
 
-        JSONObject obj = new JSONObject();
-        obj.put("alg", "RS256");
-        obj.put("typ", "JWT");
+    private static PrivateKey getPrivateKey(String privateKeyStr) throws Exception {
+        byte[] privateKeyBytes = keyStringToBytes(privateKeyStr);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+        KeyFactory kf = KeyFactory.getInstance(RSA_ALGORITH);
 
-        String input = obj.toString();
+        PrivateKey privateKey = kf.generatePrivate(keySpec);
 
-        System.out.println(obj.toString());
+        return privateKey;
+    }
 
-        String strPK = "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgE" +
-                "AAoIBAQCV3FkZ+aqOyk71\ngG1thMIM6t6mt2PfUQAwMuByZ7rK+G+Rs8V/LwscTTVtzuDpovd40m3CfqT" +
-                "uWJvU\nXpicBzwmm4mYyTCXntoRVYhyq/F960CvF6UMAVE5xYUVl1BP3U2k2YiUTSNjA/FX\nCBcpdQ61" +
-                "QlB1k1KnB8obsQ2RCkVG+XzKhUgYWq93qMzZhoE1FD3a/NtaG4UwR5Lt\nj2KK8jNIAVckCiN6JjZKPtkVTHd" +
-                "HHKf1OZ+FRgKZdMKuK63YzNtfoNg6v2QxRZjj\n2FOt+qA341E9fZTDQkkaOwIdh7UWfJDgNhWHAcIv4qFpx4TI" +
-                "no7xYzB3khGq3eCY\nhcZaYisXAgMBAAECggEAL+QAZ3AlZMdbL1Jw5ehgmP8v0whVNsbsb/q6RmVGyNlW\n0jGogi" +
-                "n2+37i/an4r0FAo5BfuiF9tJuFhPsL+wTG4LGg6b4VtMZzesmiNr8jB/H3\ntFyyhmEfY35vNsSpyjP3PaC06Gu0TM60" +
-                "P/Nab4m0fxFnpANGDRHMsUfGBZ1KIVFZ\nLb0HndfDLbBBkgmmFsQ0AagYPXLrcMcn/qmW2uCsYQ233LceWZHwDVTKNqDI" +
-                "ul1y\nZJRuZFRBaIps9GcqGZYcfY74IcPYnCucTqnHWpV/wwmXwTijm/NUN9wPu0M2Yr9U\nK7x+Q0KFQ/5GC3vDJTGgRF" +
-                "gOhb/hgoFuM8e/gDDKIQKBgQDHhZWDvMvb9gpjttiV\nLwZ3ik6LPnvUquj2NvIukEwBZgQQ+0Z/Tjn+aTIGMH" +
-                "JAG90vji3/4J6UYzOnQucf\nu9c671nyCyg5n6OVoKe4414pcfTCktQQXX8TTiXHY7S1bIGfug2Y+0WMlJhnjf" +
-                "86\no2AuQwGBE7/Ng12UewySiOXboQKBgQDASA6e3TG7mRg1DFH2zLEt6n4LS8N3+rmT\nOV9aROlx1SdUXqUhi" +
-                "DYy3T6UFdAoTbKFArZK63ySj4Ln0GcGZOEQ6nfhyFosWOCJ\nF7wY81EYueZVYMXzZyNvvvB8H34i3JUUYED4TFX" +
-                "TggFkg9G+KgBYNfP7aM21uRN2\nzXpu/HpLtwKBgQC6kuwxeIaZdgZZIE5/kq3VcvvnsdJSGFAojVECweSRGjiL" +
-                "OK2/\ntgL9KJmiuALeeC/pnGbfc8hWsj1HF4TRY/HsJo5RjKVc3S83aDfu6I7Q6sMaf/rW\nItgF0Xn6bf45+PF" +
-                "tz6mPeMicoQezLlGRH9185SOu9CzTnRgRFM+451MqgQKBgGLe\nKjZhggr2A05zkUmXopr7cegZAt8UWotq+q6Nt" +
-                "INFmL13FO2S/ltVC7JVLP0sRljC\nNj17rEgn5qWPsQnGoTtzdETyIfufj4SuYZdqtQ5DuD26Ts6C/+ObiEgqYU9HoJ" +
-                "gj\njgSeK5tfw5uNdDGVoyu4QkXJc8y251009nlO9QstAoGATEf4x6xWDCZbLdWvVuJF\nq5YIU4o6I9iphmG1L1FCKK" +
-                "BaZErKwiojQiXE+14wFYNl5RAKdyDk81j/MgKMnE8T\nV5E3/Ikt31p9wBIep4NsTuoBK436x9K73j/t5MHj1Mbld5n" +
-                "YTg6MXSY8lX0lplMX\n6lNNgRMkIqtlF47b1+Ss/kE=\n-----END PRIVATE KEY-----\n";
+    private static String sign(String content, String privateKeyString) throws Exception {
+        PrivateKey privateKey = getPrivateKey(privateKeyString);
 
-        String base64Signature = signSHA256RSA(input, strPK);
+        Signature signer = Signature.getInstance(RSA_SHA256);
+        signer.initSign(privateKey);
+        signer.update(content.getBytes("UTF-8"));
 
-        System.out.println("Signature = " + base64Signature);
+        byte[] signatureBytes = signer.sign();
 
-        System.out.println(Base64.getUrlEncoder().withoutPadding().encodeToString(obj.toString().getBytes(StandardCharsets.UTF_8)));
+        String signature = Base64.getUrlEncoder().encodeToString(signatureBytes);
 
-        Date date = new Date();
-
-        System.out.println(date.getTime()/1000 + 3600);
-        System.out.println(date.getTime()/1000);
-
-        JSONObject claim = new JSONObject();
-        claim.put("iss", "roche-3rs@rs-project-274507.iam.gserviceaccount.com");
-        claim.put("scope", "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file" +
-                "https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/spreadsheets" +
-                "https://www.googleapis.com/auth/spreadsheets.readonly");
-        claim.put("aud", "https://oauth2.googleapis.com/token");
-        claim.put("exp", date.getTime()/1000 + 3600);
-        claim.put("iat", date.getTime()/1000);
-
-        System.out.println(claim.toString());
-        System.out.println(Base64.getUrlEncoder().withoutPadding().encodeToString(claim.toString().getBytes(StandardCharsets.UTF_8)));
+        return signature;
 
     }
 
-    private static String signSHA256RSA(String input, String strPK) throws Exception {
-        String realPK = strPK.replaceAll("-----END PRIVATE KEY-----", "")
-                            .replaceAll("-----BEGIN PRIVATE KEY-----", "")
-                            .replaceAll("\n", "");
-
-        byte[] b1 = Base64.getDecoder().decode(realPK);
-        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(b1);
-        KeyFactory kf = KeyFactory.getInstance("RSA");
-
-        Signature privateSignature = Signature.getInstance("SHA256withRSA");
-        privateSignature.initSign(kf.generatePrivate(spec));
-        privateSignature.update(input.getBytes("UTF-8"));
-
-        byte[] s = privateSignature.sign();
-
-        return Base64.getUrlEncoder().encodeToString(s);
-
+    public static String signContent(String header, String claim) throws Exception {
+        String jwtContent = getBase64URLString(header) + "." + getBase64URLString(claim);
+        return jwtContent + "." + sign(jwtContent, PRIVATE_KEY);
     }
 }
